e46b3fc5d91c93e11e0dc28f10ff8b6557d0388e
Senad Djerlek <senajerlek@gmail.com>
2022-08-21 15:30:41 +0200

    Crypto

--- my-app/src/CryptoForm/TrecaKomp.js
+++ my-app/src/CryptoForm/TrecaKomp.js
@@ -1,51 +1,169 @@
+
 import React, { useState } from "react";
 import CryptoList from "./CrpytoForm";
 
 
-const Cryptolist = [
-  { id: 0, name: "Ethereum", value: 1700 },
-  { id: 1, name: "Bitcoin", value: 22000 },
-  { id: 2, name: "Cardano", value: 0.465},
-  { id: 3, name: "Dodgecoin", value: 0.071},
+const listOfCrypto = [
+  { id: 0, name: "Tether", value: 1.00 },
+  { id: 1, name: "BNB", value: 1650 },
+  { id: 2, name: "Cardano", value: 295.42 },
+  { id: 3, name: "Dogecoin", value: 0.071 },
+  { id: 4, name: "XRP", value: 0.334 },
+  { id: 5, name: "BNB", value: 291.39 },
+  { id: 6, name: "Polkadot", value: 8.07 },
 ];
 
 const CryptoForm = () => {
-    const [cryptoList, setCryptoList] = useState(CryptoList);
-    const [userData, setUserData] = useState({
-        name: "",
-        value: "",
-    })
-}
+  const [cryptoList, setCryptoList] = useState(listOfCrypto);
+  const [userData, setUserData] = useState({
+    name: "",
+    value: "",
+  });
+
+  const onAddCrypto = (event) => {
+    event.preventDefault();
+    setCryptoList((prev) => [
+      ...prev,
+      {
+        id: Math.random().toString(36).slice(2, 7),
+        name: userData.name,
+        value: +userData.value,
+      },
+    ]);
+    setUserData({
+      name: "",
+      value: "",
+    });
+  };
+
+  const deleteCrypto = (id) => {
+    const newCryptoList = cryptoList.filter((value) => value.id !== id);
+    setCryptoList(newCryptoList);
+  };
+
+  return (
+    <div>
+      <h1 className="clist  ">Crypto</h1>
+      <form className="crypto-form">
+        <label htmlFor="CryptoName"></label>
+        <input
+          type="text"
+          name="CryptoName"
+          id="CryptoName"
+          placeholder="Crypto Name"
+          required
+          value={userData.name}
+          onChange={(event) =>
+            setUserData((prev) => ({
+              ...prev,
+              name: event.target.value,
+            }))
+          }
+          ></input>
+
+        <label htmlFor="CryptoValue"></label>
+        <input
+          type="number"
+          name="CryptoValue"
+          id="CryptoValue"
+          placeholder="Crypto Value"
+          required
+          value={userData.value}
+          onChange={(event) =>
+            setUserData((prev) => ({
+              ...prev,
+              value: event.target.value,
+            }))
+          }
+        ></input>
+
+        <button className="btn1" type="submit" onClick={onAddCrypto}>
+          ADD
+        </button>
+      </form>
+
+      <h3 className="clist">Crypto Items</h3>
+
+      {cryptoList.map((value) => (
+        <CryptoList className = "crypto-list"
+          key={value.id}
+          name={value.name}
+          value={value.value}
+          deleteCrypto={() => deleteCrypto(value.id)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CryptoForm;
+
+
+
+// const Cryptolist = [
+//   { id: 0, name: "Ethereum", value: 1700 },
+//   { id: 1, name: "Bitcoin", value: 22000 },
+//   { id: 2, name: "Cardano", value: 0.465},
+//   { id: 3, name: "Dodgecoin", value: 0.071},
+// ];
 
 
 
 
-export default function CryptoCard() {
+// export default function CryptoCard() {
 
-    const [crypto, setCryptoList] = useState(Cryptolist);
+//   const onAddCrypto = (event) => {
+//     event.preventDefault();
+//     if (
+//       userData.name.trim().length === 0 ||
+//       userData.value.trim().length === 0
+//     ) {
+//       return alert("You must enter the name and value of Crypto.");
+//     } else if (+userData.value <= 0) {
+//       return alert("Value must be above 0.");
+//     }
+//     setCryptoList((prev) => [
+//       ...prev,
+//       {
+//         id: Math.random().toString(36).slice(2, 7),
+//         name: userData.name,
+//         value: +userData.value,
+//       },
+//     ]);
+//     setUserData({
+//       name: "",
+//       value: "",
+//     });
+//   };
+
+//   const deleteCrypto = (id) => {
+//     const newCryptoList = cryptoList.filter((value) => value.id !== id);
+//     setCryptoList(newCryptoList);
+//   };
+    
 
   
 
 
-  return (
-    <div className="Crypto-border">
+//   return (
+//     <div className="Crypto-border">
     
-    <h1>   Crypto Form  </h1>
-      <button
-        onClick={() =>
-          setCryptoList((prev) => [
-            ...prev,
-            { id: Math.random(), name: "Nova Valuta", price: 300 },
-          ])    
-        }
-      >
-        ADD
-      </button>
-      {crypto.map((e) => (
-        <div key={e.id}>
-          <CryptoList name={e.name} price={e.value  } />
-        </div>
-      ))}
-    </div>
-  );
-}
+//     <h1>   Crypto Form  </h1>
+//       <button
+//         onClick={() =>
+//           setCryptoList((prev) => [
+//             ...prev,
+//             { id: Math.random(), name: "Nova Valuta", price: 300 },
+//           ])    
+//         }
+//       >
+//         ADD
+//       </button>
+//       {crypto.map((e) => (
+//         <div key={e.id}>
+//           <CryptoList name={e.name} price={e.id.value} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
