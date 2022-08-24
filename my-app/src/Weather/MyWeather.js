e83405945a2bd9fcf0bfb3c5b88b743d9b664645
Senad Djerlek <senajerlek@gmail.com>
2022-08-25 00:39:30 +0200

    react weather card

--- my-app/src/Weather/MyWeather.js
+++ my-app/src/Weather/MyWeather.js
@@ -3,20 +3,34 @@ import "./Weather.css";
 
 const WeatherApp = () => {
   let [weather, setWeather] = useState(0);
-
   function updateWeather() {
-    setWeather(weather = Math.floor(Math.random() * 40 ) -20);
+    setWeather((weather = Math.floor(Math.random() * 50) - 20));
 
   }
+
+//   const getBackgroundColor = () => {
+//     let color;
+//     if (value === 0) {
+//         color = '';
+//     } else if (value >= 1 && value < 25) {
+//         color = 'red';
+//     } else if (value >= 25 && value < 50) {
+//         color = 'orange';
+//     } else if (value >= 50 && value < 90) {
+//         color = 'yellow';
+//     } else if (value >= 90) {
+//         color = 'green';
+//     }
+//     return color;
+// };
+
 
 
   return (
     <div className="kartica">
       <p>{weather}</p>
       <p>Kartica</p>
-      <button onClick={() => updateWeather()}>
-        Klikni ovde da promenis temperaturu
-      </button>
+      <button onClick={() => updateWeather()}>Refresh temp</button>
     </div>
   );
 };
