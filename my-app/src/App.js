5c1a3ef1786bbbdc24a0252f1953dc2bdfb75ca9
Senad Djerlek <senajerlek@gmail.com>
2022-08-29 18:42:20 +0200

    axois

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,130 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import DrugaKomp from "./DrugaKomp";
-import TeamCard from "./TeamCard/TeamCard";
-import CryptoForm from "./CryptoForm/CrpytoForm";
-import TrecaKomp from "./CryptoForm/TrecaKomp";
-import MyWeather from "./Weather//MyWeather";
-import Weather from "./Weather/Weather";
-import PostList from "./PostList/PostList";
-import PostItem from "./PostList/PostItem";
+import axios from "axios";
 
-// const DATA = [
-
-// ];
-
-// export default function App() {
-//   const [array, setArray] = useState(DATA);
-
-//   const generateNewWord = () => Math.random().toString(36).slice(2, 7);
-
-//   const reverseArray = () => {
-
-//     //kopija niza
-//     const _array = [...array];
-//     const reversed = _array.reverse();
-//     setArray(reversed);
-//   }
-
-//   return (
-//     <div className="card-container">
-//       <h4>Random recenice</h4>
-//       <button onClick={() => setArray((prev) => [generateNewWord(),...prev])}>
-//         Dodaj random recenicu
-//       </button>
-//       <button onClick={reverseArray}>REVERSE recenice</button>
-//       <ul>
-//         {array.map((el) => (
-//           <li key={el}> {el}</li>
-//         ))}
-//         ;
-//       </ul>
-//       <TeamCard />
-
-//     </div>
-//   );
-// }
-
-// export default function App() {
-//   // const [name, setName] = React.useState("");
-//   // const [email, setEmail] = React.useState("");
-//   // const [hobi, setHobi] = React.useState("");
-
-//   const [formValues, setFormValues] = useState({
-//     name: "",
-//     email: "",
-//     hobi: "",
-//     password: "",
-//   })
-
-//   return (
-//     // <div className="card-container">
-//       {/* <form
-//         onSubmit={(e) => {
-//           e.preventDefault();
-//           console.log(formValues);
-//           console.log(`NAME: ${formValues.name}, EMAIL: ${formValues.email}, HOBI: ${formValues.hobi}, PASSWORD: ${formValues.password}`);
-//         }}
-//       >
-//         <label htmlFor="html">Name</label>
-//         <input
-//           type="text"
-//           id="html"
-//           name="fav_language"
-
-//           value={formValues.name}
-//           onChange={(e) => setFormValues(prev => ({
-//             ...prev,
-//             name: e.target.value,
-//           }))}
-//         />
-//         <br />
-//         <label htmlFor="css">Email</label>
-//         <input
-//           type="text"
-//           id="css"
-//           name="fav_language"
-//           value={formValues.email}
-//           onChange={(e) => setFormValues(prev => ({
-//             ...prev,
-//             email: e.target.value,
-//           }))}
-//         />
-//         <br />
-//         <label htmlFor="javascript">Hobi</label>
-//         <input
-//           type="text"
-//           id="javascript"
-//           name="fav_language"
-//           value={formValues.hobi}
-//           onChange={(e) => setFormValues(prev => ({
-//             ...prev,
-//             hobi: e.target.value,
-//           }))}
-//         />
-//         {/* <label htmlFor="javascript">passwordd</label>
-//         <input className="pass"
-//           type="password"
-//           id="html"
-//           name="fav_language"
-//           value={formValues.password}
-//           onChange={(e) => setFormValues(prev => ({
-//             ...prev,
-//             password: e.target.value,
-//           }))}
-//         /> */}
-//         <br />
-//         <br />
-//         <input type="submit" defaultValue="Submit" />
-//       </form>
-//     </div> */}
-//   );
-// }
+const BASE_URL = "https://api.quotable.io";
 
 export default function App() {
-  return (
-    <PostList />
+  const [authors, setAuthors] = useState([]);
 
-    // <CryptoForm />
+  function getAuthors() {
+    axios
+      .get(`${BASE_URL}/authors?sortBy=quoteCount`)
+      .then((res) => setAuthors(res.data.results));
+  }
+  useEffect(() => {
+    getAuthors();
+  }, []);
+  return (
+    <div className="card-container">
+      {authors.length > 0 ? (
+        <div>
+          {authors.map((author) => (
+            <div key={author.id}>
+              <h4>{author.name}</h4>
+              <h5>{author.description}</h5>
+              <p>{author.bio}</p>
+              <p> Quote count : {author.quoteCount}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 }
