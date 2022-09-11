15ef873b0e89cf8756dc7e70f1f7f2ce0c5ade75
Senad Djerlek <senajerlek@gmail.com>
2022-09-11 16:36:15 +0200

    router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,61 +1,110 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+
+// import Mui from "./Mui";
+
+// const BASE_URL = "https://api.quotable.io";
+
+// export default function App() {
+//   const [authors, setAuthors] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [pagination, setPagination] = useState({});
+//   const [page, setPage] = useState(1);
+
+//   function getAuthors(page) {
+//     setLoading(true);
+//     try {
+//       axios
+//         .get(`${BASE_URL}/authors?sortBy=quoteCount&page=${page}&limit=15`)
+//         .then((res) => {
+//           console.log(res.data);
+//           setPagination({
+//             page: res.data.page,
+//             lastPage: res.data.totalPages,
+//           });
+//           setAuthors(res.data.results);
+//         });
+//     } catch (e) {
+//       console.log(e);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }
+
+//   useEffect(() => {
+//     getAuthors(page);
+//   }, [page]);
+
+//   return (
+//     <div className="card-container">
+//       <Mui />
+//       Page {pagination.page} / {pagination.lastPage}
+//       <button onClick={() => setPage((prev) => prev + 1)}>Next page </button>
+//       {!loading ? (
+//         <div>
+//           {authors.map((author) => (
+//             <div key={author._id}>
+//               <h4>{author.name}</h4>
+//               <h5>{author.description}</h5>
+//               <p>{author.bio}</p>
+//               <p>Quoute count: {author.quoteCount}</p>
+//               <hr />
+//             </div>
+//           ))}
+//         </div>
+//       ) : (
+//         <p>Loading...</p>
+//       )}
+//     </div>
+//   );
+// }
+import React from "react";
 import "./App.css";
-import axios from "axios";
+import BottomNavigation from "@mui/material/BottomNavigation";
 
-import Mui from "./Mui";
-
-const BASE_URL = "https://api.quotable.io";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import HomePage from "./Pages/Homepage/Homepage";
+import TeamPage from "./Pages/Teampage/Teampage";
 
 export default function App() {
-  const [authors, setAuthors] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [pagination, setPagination] = useState({});
-  const [page, setPage] = useState(1);
+  let activeStyle = {
+    textDecoration: "underline",
+    color: "red",
+  };
 
-  function getAuthors(page) {
-    setLoading(true);
-    try {
-      axios
-        .get(`${BASE_URL}/authors?sortBy=quoteCount&page=${page}&limit=15`)
-        .then((res) => {
-          console.log(res.data);
-          setPagination({
-            page: res.data.page,
-            lastPage: res.data.totalPages,
-          });
-          setAuthors(res.data.results);
-        });
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    getAuthors(page);
-  }, [page]);
+  let activeClassName = "underline";
 
   return (
     <div className="card-container">
-      <Mui />
-      Page {pagination.page} / {pagination.lastPage}
-      <button onClick={() => setPage((prev) => prev + 1)}>Next page </button>
-      {!loading ? (
-        <div>
-          {authors.map((author) => (
-            <div key={author._id}>
-              <h4>{author.name}</h4>
-              <h5>{author.description}</h5>
-              <p>{author.bio}</p>
-              <p>Quoute count: {author.quoteCount}</p>
-              <hr />
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1>Welcome to React Router!</h1>
+      <BottomNavigation showLabels>
+        <NavLink
+          className={({ isActive }) => (isActive ? activeClassName : undefined)}
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          <h4>Home </h4>
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          <h4>About </h4>
+        </NavLink>
+        <NavLink
+          to="/team"
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        >
+          <h4>Team </h4>
+        </NavLink>
+      </BottomNavigation>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<h1>ABOUT PAGE</h1>} />
+        <Route path="team" element={<TeamPage />} />
+      </Routes>
     </div>
   );
 }
