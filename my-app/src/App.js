a6270de05ca35fca2013c61f7cfb1d365f6c2376
Senad Djerlek <senajerlek@gmail.com>
2022-09-12 23:06:42 +0200

    react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,72 +1,11 @@
-// import React, { useEffect, useState } from "react";
-// import "./App.css";
-// import axios from "axios";
-
-// import Mui from "./Mui";
-
-// const BASE_URL = "https://api.quotable.io";
-
-// export default function App() {
-//   const [authors, setAuthors] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [pagination, setPagination] = useState({});
-//   const [page, setPage] = useState(1);
-
-//   function getAuthors(page) {
-//     setLoading(true);
-//     try {
-//       axios
-//         .get(`${BASE_URL}/authors?sortBy=quoteCount&page=${page}&limit=15`)
-//         .then((res) => {
-//           console.log(res.data);
-//           setPagination({
-//             page: res.data.page,
-//             lastPage: res.data.totalPages,
-//           });
-//           setAuthors(res.data.results);
-//         });
-//     } catch (e) {
-//       console.log(e);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-
-//   useEffect(() => {
-//     getAuthors(page);
-//   }, [page]);
-
-//   return (
-//     <div className="card-container">
-//       <Mui />
-//       Page {pagination.page} / {pagination.lastPage}
-//       <button onClick={() => setPage((prev) => prev + 1)}>Next page </button>
-//       {!loading ? (
-//         <div>
-//           {authors.map((author) => (
-//             <div key={author._id}>
-//               <h4>{author.name}</h4>
-//               <h5>{author.description}</h5>
-//               <p>{author.bio}</p>
-//               <p>Quoute count: {author.quoteCount}</p>
-//               <hr />
-//             </div>
-//           ))}
-//         </div>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// }
 import React from "react";
 import "./App.css";
 import BottomNavigation from "@mui/material/BottomNavigation";
 
 import { Routes, Route, Link, NavLink } from "react-router-dom";
-import HomePage from "./Pages/Homepage/Homepage";
-import TeamPage from "./Pages/Teampage/Teampage";
-import About from "./Pages/Aboutpage/About";
+import HomePage from "./pages/HomePage/HomePage";
+import NewsPage from "./pages/NewsPage/NewsPage";
+import SingleNews from "./pages/NewsPage/SingleNews";
 
 export default function App() {
   let activeStyle = {
@@ -87,24 +26,19 @@ export default function App() {
         >
           <h4>Home </h4>
         </NavLink>
+
         <NavLink
-          to="/about"
+          to="/news"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
         >
-          <h4>About </h4>
-        </NavLink>
-        <NavLink
-          to="/team"
-          style={({ isActive }) => (isActive ? activeStyle : undefined)}
-        >
-          <h4>Team </h4>
+          <h4>News </h4>
         </NavLink>
       </BottomNavigation>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<About />} />
-        <Route path="team" element={<TeamPage />} />
+        <Route path="news" element={<NewsPage />} />
+        <Route path="news/:id" element={<SingleNews />} />
       </Routes>
     </div>
   );
