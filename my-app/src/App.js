c75d1c5fb50c3137b6e03d734c18b681fc96262f
Senad Djerlek <senajerlek@gmail.com>
2022-09-19 22:10:13 +0200

    useContext

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,69 @@
-import React from "react";
+// import React from "react";
+// import "./App.css";
+// import BottomNavigation from "@mui/material/BottomNavigation";
+
+// import { Routes, Route, Link, NavLink } from "react-router-dom";
+// import HomePage from "./pages/HomePage/HomePage";
+// import NewsPage from "./pages/NewsPage/NewsPage";
+// import SingleNews from "./pages/NewsPage/SingleNews";
+
+// export default function App() {
+//   let activeStyle = {
+//     textDecoration: "underline",
+//     color: "red",
+//   };
+
+//   let activeClassName = "underline";
+
+//   return (
+//     <div className="card-container">
+//       <h1>Welcome to React Router!</h1>
+//       <BottomNavigation showLabels>
+//         <NavLink
+//           className={({ isActive }) => (isActive ? activeClassName : undefined)}
+//           to="/"
+//           style={({ isActive }) => (isActive ? activeStyle : undefined)}
+//         >
+//           <h4>Home </h4>
+//         </NavLink>
+
+//         <NavLink
+//           to="/news"
+//           style={({ isActive }) => (isActive ? activeStyle : undefined)}
+//         >
+//           <h4>News </h4>
+//         </NavLink>
+//       </BottomNavigation>
+
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="news" element={<NewsPage />} />
+//         <Route path="news/:id" element={<SingleNews />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+import React, { createContext } from "react";
 import "./App.css";
 import BottomNavigation from "@mui/material/BottomNavigation";
 
 import { Routes, Route, Link, NavLink } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import NewsPage from "./pages/NewsPage/NewsPage";
-import SingleNews from "./pages/NewsPage/SingleNews";
+import HomePage from "./Pages/Homepage/Homepage";
+import NewsPage from "./Pages/NewsPage/NewsPage";
+import SingleNews from "./Pages/NewsPage/SingleNews";
+
+export const UserContext = createContext();
 
 export default function App() {
+  const [user, setUser] = React.useState({
+    name: "",
+    email: "",
+    address: "",
+    role: "",
+  });
+
+  const value = { user, setUser };
+
   let activeStyle = {
     textDecoration: "underline",
     color: "red",
@@ -35,11 +91,13 @@ export default function App() {
         </NavLink>
       </BottomNavigation>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="news" element={<NewsPage />} />
-        <Route path="news/:id" element={<SingleNews />} />
-      </Routes>
+      <UserContext.Provider value={value}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="news" element={<NewsPage />} />
+          <Route path="news/:id" element={<SingleNews />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ my-app/src/NewPage/MoviePage.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const MoviePage = () => {
+
+    return(
+        <div>
+            
+        </div>
+    )
+};
+
+
+export default MoviePage;
